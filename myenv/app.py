a91e80b5a6cb99cc2978989114e6a07012388be8
jozo252 <81568511+jozo252@users.noreply.github.com>
2025-02-15 18:09:33 +0100

    Added Streamlit app file

---
+++ myenv/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Multi-Page App", layout="wide")
+
+st.title("Servus kamo bracho")
+
+st.write("Use the sidebar to navigate between pages.")
